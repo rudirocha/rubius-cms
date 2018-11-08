4511ea58465dbd4d9f85c91dc16eab018cd28f98
Rudi Rocha <rudi.rocha@gmail.com>
2018-11-08 21:13:43 +0000

    First commit

---
+++ assets/admin/js/app.js
@@ -0,0 +1,1 @@
+require('../scss/material-dashboard.scss')
---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function index()
+    {
+        return $this->render('admin/dashboard/index.html.twig', [
+            'pageTitle' => 'Dashboard',
+        ]);
+    }
+}
